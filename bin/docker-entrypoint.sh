92a3a981a2b4cff0e9ce2acfee5651406b5869f5
Mark Schenk <m.m.a.schenk@tudelft.nl>
2021-01-25 10:13:53 +0100

    use wwwdata-bash instead of su

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -42,7 +42,7 @@ gpg_gen_key() {
 
   entropy_check
 
-  su -c "gpg --batch --no-tty --gen-key <<EOF
+  wwwdata-bash -c "gpg --batch --no-tty --gen-key <<EOF
     Key-Type: default
 		Key-Length: $key_length
 		Subkey-Type: default
@@ -52,15 +52,15 @@ gpg_gen_key() {
     Expire-Date: $expiration
     %no-protection
 		%commit
-EOF" -ls /bin/bash www-data
+EOF"
 
-  su -c "gpg --armor --export-secret-keys $key_email > $gpg_private_key" -ls /bin/bash www-data
-  su -c "gpg --armor --export $key_email > $gpg_public_key" -ls /bin/bash www-data
+  wwwdata-bash -c "gpg --armor --export-secret-keys $key_email > $gpg_private_key"
+  wwwdata-bash -c "gpg --armor --export $key_email > $gpg_public_key"
 }
 
 gpg_import_key() {
-  su -c "gpg --batch --import $gpg_public_key" -ls /bin/bash www-data
-  su -c "gpg --batch --import $gpg_private_key" -ls /bin/bash www-data
+  wwwdata-bash -c "gpg --batch --import $gpg_public_key"
+  wwwdata-bash -c "gpg --batch --import $gpg_private_key"
 }
 
 gen_ssl_cert() {
@@ -73,22 +73,22 @@ install() {
   local app_config="/var/www/passbolt/config/app.php"
 
   if [ ! -f "$app_config" ]; then
-    su -c 'cp /var/www/passbolt/config/app.default.php /var/www/passbolt/config/app.php' -s /bin/bash www-data
+    wwwdata-bash -c 'cp /var/www/passbolt/config/app.default.php /var/www/passbolt/config/app.php'
   fi
 
   if [ -z "${PASSBOLT_GPG_SERVER_KEY_FINGERPRINT+xxx}" ] && [ ! -f  '/var/www/passbolt/config/passbolt.php' ]; then
-    gpg_auto_fingerprint="$(su -c "gpg --list-keys --with-colons ${PASSBOLT_KEY_EMAIL:-passbolt@yourdomain.com} |grep fpr |head -1| cut -f10 -d:" -ls /bin/bash www-data)"
+    gpg_auto_fingerprint="$(wwwdata-bash -c "gpg --list-keys --with-colons ${PASSBOLT_KEY_EMAIL:-passbolt@yourdomain.com} |grep fpr |head -1| cut -f10 -d:")"
     export PASSBOLT_GPG_SERVER_KEY_FINGERPRINT=$gpg_auto_fingerprint
   fi
 
-  su -c '/var/www/passbolt/bin/cake passbolt install --no-admin' -s /bin/bash www-data || su -c '/var/www/passbolt/bin/cake passbolt migrate' -s /bin/bash www-data && echo "Enjoy! ☮"
+  wwwdata-bash -c '/var/www/passbolt/bin/cake passbolt install --no-admin' || wwwdata-bash -c '/var/www/passbolt/bin/cake passbolt migrate' && echo "Enjoy! ☮"
 }
 
 email_cron_job() {
   cron_task='/etc/cron.d/passbolt_email'
   declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /etc/environment
   if [ ! -f "$cron_task" ]; then
-    echo "* * * * * su -c \"source /etc/environment ; /var/www/passbolt/bin/cake EmailQueue.sender\" -s /bin/bash www-data >> /var/log/cron.log 2>&1" >> $cron_task
+    echo "* * * * * wwwdata-bash -c \"source /etc/environment ; /var/www/passbolt/bin/cake EmailQueue.sender\" >> /var/log/cron.log 2>&1" >> $cron_task
     crontab /etc/cron.d/passbolt_email
   fi
 }
